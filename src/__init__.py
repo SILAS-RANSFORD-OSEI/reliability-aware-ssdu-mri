9f9105a4dfe8a8f00878848367b5e499b07faa52
SILAS-RANSFORD-OSEI <silasransfordosei@gmail.com>
2026-05-11 11:11:30 +0000

    Add package initializer

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+Reliability-aware self-supervised MRI reconstruction package.
+
+This package contains utilities for MRI reconstruction, undersampling,
+evaluation, and later uncertainty-aware deep learning models.
+"""
