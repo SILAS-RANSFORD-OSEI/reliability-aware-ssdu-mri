d16c1799f74b5d34477a12e6327fb97cc648e4e8
SILAS-RANSFORD-OSEI <silasransfordosei@gmail.com>
2026-05-15 12:33:41 +0000

    Import HeteroscedasticCNNReconstructor in __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,3 +4,4 @@ Model definitions for MRI reconstruction.
 
 from .simple_cnn import SimpleCNNReconstructor
 from .dropout_cnn import DropoutCNNReconstructor
+from .heteroscedastic_cnn import HeteroscedasticCNNReconstructor
